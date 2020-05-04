0e2a96b0ec8f7c878260acf3d73b71bcf86a03a6
Dirsha <54706292+Dirsha@users.noreply.github.com>
2020-05-04 15:32:11 +0300

    added component main to app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import LeftMenu from "./components/LeftMenu";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <LeftMenu />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <h1 className="main-title">Main</h1>
+    </div>
+  );
+};
+
+export default Main;
